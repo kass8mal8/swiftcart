614b51d993a1599480dfadc32c7a84d6ce25dde7
kass8mal8 <kassimaly21@gmail.com>
2024-04-24 15:21:23 +0300

    minor changes

--- client/src/components/authentication/Form.jsx
+++ client/src/components/authentication/Form.jsx
@@ -115,7 +115,7 @@ const Form = () => {
                             </InputAdornment>
                     }}
                 />
-                { location.pathname === '/signin' && 
+                { ['/signin', '/'].includes(location.pathname) && 
                     <Stack direction='row' spacing={1} sx={{ alignItems: 'center', marginLeft: '44%'}}>
                         <Typography variant='body2'>Don't have an account?</Typography>
                         <ArrowRightAlt sx={{ color: '#ff5c00' }} onClick={() => navigate('/signup')} /> 
